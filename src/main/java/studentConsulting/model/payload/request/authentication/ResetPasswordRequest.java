5888f80474845f602568ef8cf3bd692cfa81c6d8
quangnghia1110 <ngoquangnghia111003@gmail.com>
2024-09-13 14:08:43 +0700

    Fix lỗi api change email khi đăng ký và nhập mật khẩu mới khi quên mật khẩu

--- src/main/java/studentConsulting/model/payload/request/authentication/ResetPasswordRequest.java
+++ src/main/java/studentConsulting/model/payload/request/authentication/ResetPasswordRequest.java
@@ -4,6 +4,8 @@ import lombok.Data;
 
 @Data
 public class ResetPasswordRequest {
-    private String email;
+	private String email;
+    private String oldPassword;
     private String newPassword;
+    private String repeatPassword;
 }
