13830e2abb31700912499cafcf0559b8c90c8ed8
quangnghia1110 <ngoquangnghia111003@gmail.com>
2024-09-13 18:46:44 +0700

    update

--- src/main/java/studentConsulting/model/payload/dto/UserInformationDTO.java
+++ src/main/java/studentConsulting/model/payload/dto/UserInformationDTO.java
@@ -1,10 +1,14 @@
 package studentConsulting.model.payload.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserInformationDTO {
     private Integer id;
     private String username;
