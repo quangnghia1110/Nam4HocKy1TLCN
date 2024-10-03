cee5d5e1f41f8a0bae345b76e06f2900ff01692c
quangnghia1110 <ngoquangnghia111003@gmail.com>
2024-10-03 21:52:36 +0700

    thực api list notification của người dùng

---
+++ src/main/java/studentConsulting/model/payload/dto/notification/NotificationDTO.java
@@ -0,0 +1,24 @@
+package studentConsulting.model.payload.dto.notification;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class NotificationDTO {
+    private Integer id;
+    private SenderDTO sender;
+    private String content;
+    private LocalDateTime time;
+
+    @Data
+    @Builder
+    public static class SenderDTO {
+        private Integer id;
+        private String email;
+        private String fullName;
+    }
+}
+
--- src/main/java/studentConsulting/repository/notification/NotificationRepository.java
+++ src/main/java/studentConsulting/repository/notification/NotificationRepository.java
@@ -1,13 +1,14 @@
 package studentConsulting.repository.notification;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import studentConsulting.model.entity.notification.NotificationEntity;
 
 import java.util.List;
 
 @Repository
-public interface NotificationRepository extends JpaRepository<NotificationEntity, Integer> {
+public interface NotificationRepository extends PagingAndSortingRepository<NotificationEntity, Integer>, JpaSpecificationExecutor<NotificationEntity> {
 
     List<NotificationEntity> findByReceiverId(Integer receiverId);
 
