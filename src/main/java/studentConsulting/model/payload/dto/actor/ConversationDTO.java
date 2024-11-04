2077ef5e8c516f297f02a6d89fa40b01f63ee2a3
quangnghia1110 <ngoquangnghia111003@gmail.com>
2024-11-04 13:25:59 +0700

    thực hiện refactor code

---
+++ src/main/java/studentConsulting/model/payload/dto/actor/ConversationDTO.java
@@ -0,0 +1,22 @@
+package studentConsulting.model.payload.dto.actor;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ConversationDTO {
+    private Integer id;
+    private DepartmentDTO department;
+    private String name;
+    private Boolean isGroup;
+    private LocalDate createdAt;
+    private List<MemberDTO> members;
+}
