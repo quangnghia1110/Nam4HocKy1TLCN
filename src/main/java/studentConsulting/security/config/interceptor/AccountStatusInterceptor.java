baa7d5aa4c4c9ed966db39518a242cf04c0780eb
Quang Nghĩa <89003923+quangnghia1110@users.noreply.github.com>
2024-12-27 15:37:59 +0700

    Update AccountStatusInterceptor.java

--- src/main/java/studentConsulting/security/config/interceptor/AccountStatusInterceptor.java
+++ src/main/java/studentConsulting/security/config/interceptor/AccountStatusInterceptor.java
@@ -1,36 +1,36 @@
-package studentConsulting.security.config.interceptor;
+// package studentConsulting.security.config.interceptor;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-import studentConsulting.model.entity.AccountEntity;
-import studentConsulting.model.exception.Exceptions;
-import studentConsulting.repository.admin.AccountRepository;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.core.context.SecurityContextHolder;
+// import org.springframework.stereotype.Component;
+// import org.springframework.web.servlet.HandlerInterceptor;
+// import studentConsulting.model.entity.AccountEntity;
+// import studentConsulting.model.exception.Exceptions;
+// import studentConsulting.repository.admin.AccountRepository;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
 
-@Component
-public class AccountStatusInterceptor implements HandlerInterceptor {
+// @Component
+// public class AccountStatusInterceptor implements HandlerInterceptor {
 
-    @Autowired
-    private AccountRepository accountRepository;
+//     @Autowired
+//     private AccountRepository accountRepository;
 
-//    @Override
-//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-//        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-//
-//        AccountEntity account = accountRepository.findByEmail(email)
-//                .orElseThrow(() -> new Exceptions.ErrorException("Tài khoản không tồn tại"));
-//
-//        if (!account.isActivity()) {
-//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//            response.getWriter().write("Tài khoản của bạn đã bị khóa. Vui lòng đăng nhập lại.");
-//            return false;
-//        }
-//
-//        return true;
-//    }
-}
+// //    @Override
+// //    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+// //        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+// //
+// //        AccountEntity account = accountRepository.findByEmail(email)
+// //                .orElseThrow(() -> new Exceptions.ErrorException("Tài khoản không tồn tại"));
+// //
+// //        if (!account.isActivity()) {
+// //            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+// //            response.getWriter().write("Tài khoản của bạn đã bị khóa. Vui lòng đăng nhập lại.");
+// //            return false;
+// //        }
+// //
+// //        return true;
+// //    }
+// }
 
