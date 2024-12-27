05a1ae8ca54cbc7fb645f2114c49b112199bfc07
Quang Nghĩa <89003923+quangnghia1110@users.noreply.github.com>
2024-12-27 15:33:17 +0700

    Update WebConfig.java

--- src/main/java/studentConsulting/security/config/interceptor/WebConfig.java
+++ src/main/java/studentConsulting/security/config/interceptor/WebConfig.java
@@ -1,21 +1,21 @@
-package studentConsulting.security.config.interceptor;
+// package studentConsulting.security.config.interceptor;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.ForwardedHeaderFilter;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.filter.ForwardedHeaderFilter;
+// import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+// @Configuration
+// public class WebConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private AccountStatusInterceptor accountStatusInterceptor;
+//     @Autowired
+//     private AccountStatusInterceptor accountStatusInterceptor;
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(accountStatusInterceptor)
-                .addPathPatterns("/api/v1/**");
-    }
-}
+//     @Override
+//     public void addInterceptors(InterceptorRegistry registry) {
+//         registry.addInterceptor(accountStatusInterceptor)
+//                 .addPathPatterns("/api/v1/**");
+//     }
+// }
